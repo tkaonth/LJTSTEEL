88f4afd13c0643a52c0951253d9930a991448797
TKAON <tkaonth@gmail.com>
2023-11-17 17:03:27 +0700

    Update DailyReport.php

--- api/DailyReport.php
+++ api/DailyReport.php
@@ -44,21 +44,19 @@ $endDateFormat = date('Y-m-d', strtotime(str_replace('/', '-', $endDate)));
 // GROUP BY Se_Date, Se_SellNo, Se_BranchID, Se_CustomerID, Cs_Customer, Bn_Branch_TH, Bn_Branch_LA, Bn_Branch_EN, Dp_DepositID, Dp_Amount, (Ss_Paid - Dp_Amount), Ss_Paid, Ss_InvoiceNO, Ss_Discount, Ss_Total,Ss_Grand_Total, Ss_Remaining, Se_Invoice_Date, Se_InvoiceNO, Ss_Payment_Medthod, Di_Date, Di_Delivery_ID, Ss_Bank
 // ORDER BY Se_Invoice_Date";
 
-$query = "SELECT CONVERT(DATE, Se_Date) As i_Date, Se_SellNo, SUBSTRING(Se_SellNo, 1, 2) As i_Doc, 'Sell' As i_Document, Se_BranchID, Bn_Branch_TH, Bn_Branch_LA, Bn_Branch_EN, Cs_Customer, Ss_Grand_Total, Ss_Total As i_Total, IIF(Dp_DepositID IS NULL, Ss_Paid, 0) As Ss_Paid, IIF(Ss_Paid = 0, '-', Ss_Payment_Medthod) AS i_Paid, Ss_Remaining, Dp_DepositID, Di_Delivery_ID
-FROM tbl_Sell LEFT OUTER JOIN tbl_Branch ON Se_BranchID = Bn_ID LEFT OUTER JOIN tbl_Customers ON Se_CustomerID = Cs_ID LEFT OUTER JOIN tbl_Sell_Summary ON Se_InvoiceNO = Ss_InvoiceNO LEFT OUTER JOIN tbl_Deposit ON Se_SellNo = Dp_SellNo LEFT OUTER JOIN tbl_Delivery_Invoice ON Se_SellNo = Di_InvoiceID
-WHERE CONVERT(DATE, Se_Date) BETWEEN '$startDateFormat' AND '$endDateFormat'  AND Dp_DepositID IS NULL
-GROUP BY Se_Date, Se_SellNo, SUBSTRING(Se_SellNo, 1, 2), Se_BranchID, Bn_Branch_TH, Bn_Branch_LA, Bn_Branch_EN, Cs_Customer, Ss_Grand_Total, Ss_Total, Ss_Paid, Ss_Payment_Medthod, Ss_Remaining, Dp_DepositID, Di_Delivery_ID
-UNION
-SELECT CONVERT(DATE, Dp_Date) As i_Date, Dp_DepositID, SUBSTRING(Dp_DepositID, 1, 2) As i_Doc, 'Deposit' As i_Document, Se_BranchID, Bn_Branch_TH, Bn_Branch_LA, Bn_Branch_EN, Cs_Customer, Ss_Grand_Total, Dp_Amount, 0 As i_Total, IIF(Dp_Amount = 0, '-', Ss_Payment_Medthod) AS i_Paid, Ss_Remaining, Dp_DepositID, Di_Delivery_ID
-FROM tbl_Sell LEFT OUTER JOIN tbl_Branch ON Se_BranchID = Bn_ID LEFT OUTER JOIN tbl_Customers ON Se_CustomerID = Cs_ID LEFT OUTER JOIN tbl_Sell_Summary ON Se_InvoiceNO = Ss_InvoiceNO LEFT OUTER JOIN tbl_Deposit ON Se_SellNo = Dp_SellNo LEFT OUTER JOIN tbl_Delivery_Invoice ON Se_SellNo = Di_InvoiceID 
-WHERE Se_Date BETWEEN '$startDateFormat' AND '$endDateFormat'  AND Dp_DepositID IS NOT NULL
-GROUP BY CONVERT(DATE, Dp_Date), Dp_DepositID, SUBSTRING(Dp_DepositID, 1, 2), Se_BranchID, Bn_Branch_TH, Bn_Branch_LA, Bn_Branch_EN, Cs_Customer, Ss_Grand_Total, Dp_Amount, Ss_Payment_Medthod, Ss_Remaining, Dp_DepositID, Di_Delivery_ID
-UNION
-SELECT CONVERT(DATE, Se_Invoice_Date) As i_Date, Se_InvoiceNO, SUBSTRING(Se_InvoiceNO, 1, 2) As i_Doc, 'Invoice' As i_Document, Se_BranchID, Bn_Branch_TH, Bn_Branch_LA, Bn_Branch_EN, Cs_Customer, Ss_Grand_Total, 0 As i_Total, (Ss_Paid - Dp_Amount) As i_Amount, IIF(Dp_Amount = 0, '-', Ss_Payment_Medthod) AS i_Paid, Ss_Remaining, Dp_DepositID, Di_Delivery_ID
-FROM tbl_Sell LEFT OUTER JOIN tbl_Branch ON Se_BranchID = Bn_ID LEFT OUTER JOIN tbl_Customers ON Se_CustomerID = Cs_ID LEFT OUTER JOIN tbl_Sell_Summary ON Se_InvoiceNO = Ss_InvoiceNO LEFT OUTER JOIN tbl_Deposit ON Se_SellNo = Dp_SellNo LEFT OUTER JOIN tbl_Delivery_Invoice ON Se_SellNo = Di_InvoiceID 
-WHERE Se_Date BETWEEN '$startDateFormat' AND '$endDateFormat'  AND Dp_DepositID IS NOT NULL
-GROUP BY CONVERT(DATE, Se_Invoice_Date), Se_InvoiceNO, SUBSTRING(Se_InvoiceNO, 1, 2), Se_BranchID, Bn_Branch_TH, Bn_Branch_LA, Bn_Branch_EN, Cs_Customer, Ss_Grand_Total, (Ss_Paid - Dp_Amount), Dp_Amount, Ss_Payment_Medthod, Ss_Remaining, Dp_DepositID, Di_Delivery_ID
-ORDER BY i_Date";
+$query = "SELECT Ss_CustomerID,Ss_InvoiceNo,Ss_Price,Ss_Vat,Ss_Total,Ss_Discount,Ss_Grand_Total,Ss_Paid,Ss_Remaining,Ss_Payment_Medthod,Se_Qaotation,
+Bn_Branch_TH,Bn_Branch_LA,Bn_Branch_EN,Di_Delivery_ID,Cs_Customer,Dp_Date,Dp_DepositID,Dp_Amount,CONVERT(varchar(10),Ss_date,105 ) AS i_Date 
+FROM tbl_Sell_Summary 
+LEFT OUTER JOIN ( 
+SELECT Se_InvoiceNo,Se_SellNo,Se_Qaotation,Se_BranchID,Se_UserId
+FROM tbl_Sell
+GROUP BY Se_InvoiceNo,Se_SellNo,Se_Qaotation,Se_BranchID,Se_UserId) AS tbl_Sell ON Ss_InvoiceNO = Se_SellNo 
+LEFT OUTER JOIN tbl_Branch ON Se_BranchID = Bn_ID LEFT OUTER JOIN ( 
+SELECT Di_Delivery_ID,Di_InvoiceID
+FROM tbl_Delivery_Invoice
+GROUP BY Di_Delivery_ID,Di_InvoiceID) AS tbl_Delivery_Invoice ON Se_InvoiceNO = Di_delivery_ID
+LEFT OUTER JOIN tbl_Customers ON Ss_CustomerID = Cs_ID LEFT OUTER JOIN tbl_Deposit ON Dp_SellNo = Se_InvoiceNO
+WHERE CONVERT(DATE,Ss_date) BETWEEN '2023-11-17' AND '2023-11-17'";
 
 // Execute the query
 $result = sqlsrv_query($conn, $query);
